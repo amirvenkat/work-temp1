6032d3db2a9f741daa1ee060cf5ca86664e65a7f
amirvenkat <amirvenkat@yahoo.com>
2023-02-13 01:09:04 +0530

    www

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("test12ssddd3");
+		System.out.println("test12ssdddddd3");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
